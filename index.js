8f7c66d597727bf4b9bd627c16dd63d32460c4ed
bpoole53 <bpoole53@gmail.com>
2023-09-08 17:21:34 -0500

    added the recommended directories and files from the homework page, also added the correct inquiries

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const fs = require('fs');
 inquirer.prompt([
     {
     type: 'input',
-    message: 'Please enter your project title.',
-    name: 'title',
+    message: 'Please enter 3 letters to put in the logo.',
+    name: 'text',
     },
-
+    {
+    type: 'input',
+    message: 'Please enter the text color',
+    name: 'textcolor',
+    },  
+    {
+    type: 'list',
+    message: 'Please choose a shape',
+    name: 'shape',
+    choices: ['circle', 'triangle', 'square'],
+    },
+    {
+    type: 'input',
+    message: 'Please enter the shape color',
+    name: 'shapecolor',
+    },
 ])
