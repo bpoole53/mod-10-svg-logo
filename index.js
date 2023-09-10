fa3249fa3f5de553060368bfd492751d4da5685f
bpoole53 <bpoole53@gmail.com>
2023-09-10 13:14:10 -0500

    got an svg file to be written, but the image did not come out right

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Shape, Triangle, Circle, Square} = require ('./lib/shape')
 
 inquirer.prompt([
     {
@@ -16,7 +17,7 @@ inquirer.prompt([
     type: 'list',
     message: 'Please choose a shape',
     name: 'shape',
-    choices: ['circle', 'triangle', 'square'],
+    choices: ['Circle', 'Triangle', 'Square'],
     },
     {
     type: 'input',
@@ -24,4 +25,18 @@ inquirer.prompt([
     name: 'shapeColor',
     },
 ])
-    
+.then(answers => {
+    console.log(answers.shape)
+    let shape;
+    if(answers.shape === 'Triangle'){
+        shape = new Triangle(answers.shapeColor);
+    }
+    console.log(shape)
+    const svgGenerate = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"> ${shape.render()} <text x="30" y="30" font-size="40" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text></svg>`
+
+    fs.writeFileSync('logo.svg', svgGenerate, (err) =>
+    err ? console.error(err) : console.log('Success!'));
+})
+.catch(error => {
+    console.error(error);
+})
